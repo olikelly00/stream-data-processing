f816ebc366ff6f6e939c1efe04e32f75be4fc9de
Oli Kelly <olikelly@MASTUDENT-Oli-Kelly-NJ9KFPW96H.local>
2025-02-25 10:07:59 +0000

    cleanup

--- producer.py
+++ producer.py
@@ -3,45 +3,32 @@ import socket
 import random
 import json
 import time
-
+import uuid
+import logging
 
 
 # Define the Kafka configuration settings to be passed to the producer
-conf = {
-    # Kafka broker(s) to which the producer connects. In a multi-broker setup, this acts as the entry point to the Kafka cluster    
+kafka_producer_config = {
     'bootstrap.servers': 'localhost:9092',
-    # Unique id for the producer
     'client.id': socket.gethostname()
 }
-order_id = 0
-# Create producer instance and pass it parameters
-producer = Producer(conf)
 
-customer_ids = list(range(1,20))
 
-# Fetch topic metadata - used later to get partition count
-
+topic_name = "online-orders"
+producer = Producer(kafka_producer_config)
 topic_data = producer.list_topics()
 topic_name = "online-orders"
-
+customer_ids = list(range(1,20))
 
 if topic_name not in topic_data.topics:
         raise Exception(f"Error: Topic {topic_name} does not exist. Must be created first using this CLI command: bin/kafka-topics.sh --create --topic {topic_name} --bootstrap-server localhost:9092")
 
-def produce_event(topic_name):
-    """
-    Produces a message to the specified Kafka topic.
+def produce_event(topic_name):    
 
-    Args:
-        topic_name (str): The name of the Kafka topic.
-        message (str): The message payload.
-    """
-    global order_id
-    
+    order_id = str(uuid.uuid1())
     amount = random.randint(2, 30)
-    
     customer_id = customer_ids[random.randint(0, len(customer_ids) - 1)]
-        
+    
     json_message = json.dumps({
         "order_id": order_id,
         "amount": amount,
@@ -49,17 +36,8 @@ def produce_event(topic_name):
         "total_orders": len(topic_data.topics)
     })
 
-
-
     partition_count = len(topic_data.topics[topic_name].partitions)
 
-
-    # Call the in-built produce function on the producer instance, passing it the topic name, key and value
-    
-    # Key = optional. If provided, Kafka hashes it to determine the partition, ensuring all messages with the same key go to the same partition.
-    # Value =  the actual message (payload)
-    # Partition = manually specifying a partition overrides Kafka's default partitioning logic. 
-    #   If not provided, Kafka will either use the key to determine a partition or distribute messages evenly.
 
     producer.produce(
         topic_name, 
@@ -67,12 +45,19 @@ def produce_event(topic_name):
         value=json_message.encode("utf-8"),
         partition=random.randint(0, partition_count-1))
     
-    # Explicitly flush the message so that all buffered messages are immediately sent to the cluster 
-    producer.flush()
-    order_id += 1 
-    time.sleep(10)
+    producer.flush() 
+    logging.info(f"✅ Order Sent: {json_message}")
 
-while True:
-    # Should work as expected
-    produce_event('online-orders')
+
+def main():
+    try:
+        while True:
+            produce_event()
+            time.sleep(10)  # Adjust the interval as needed
+    except KeyboardInterrupt:
+        logging.info("Producer stopped.")
+
+if __name__ == "__main__":
+    logging.info("Starting Kafka Producer...")
+    main()
     
--- subscriber.py
+++ subscriber.py
@@ -1,11 +1,15 @@
 from confluent_kafka import Consumer, KafkaError, KafkaException
 import sys
 import time
+import logging
 import json
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 running = True
 
-conf = {
+kafka_consumer_config = {
     'bootstrap.servers': 'localhost:9092',
     #  Group ID (mandatory) - specify which consumer group the consumer is a member of.
     'group.id': 'foo',
@@ -14,8 +18,8 @@ conf = {
     'enable.auto.commit': True 
 }
 
-consumer = Consumer(conf)
 
+# Initialise global statistics
 order_total = 0 
 order_sum = 0
 order_amounts = []
@@ -24,43 +28,41 @@ customer_order_summary = {}
 def msg_process(msg):
     msg_payload = msg.value().decode("utf-8")
 
-    global order_total
-    global order_sum
-    global order_amounts
-    global customer_order_summary
+    global order_total, order_sum, order_amounts, customer_order_summary
 
-    payload_dict = json.loads(msg_payload)
-
-    order_id = str(payload_dict["order_id"])
-    amount = payload_dict["amount"]
-    customer_id = str(payload_dict["customer_id"])
-    order_total += 1
-    order_sum += int(amount)
-    order_amounts.append(amount)
-    if customer_id in customer_order_summary.keys():
-        customer_order_summary[f"Sales for customer ID: {customer_id}"] += 1
-    else:
-        customer_order_summary[f"Sales for customer ID {customer_id}"] = 1
-
-    """Prints the received Kafka message."""
-    print(f"""
-        New Event Received: {msg}\n
-        Order ID: {order_id}\n
-        Customer ID: {customer_id}\n
-        Amount: {amount}\n\n
-        Summary Stats:\n
-        The total orders received: {order_total}\n
-        The total amount of all orders received so far: {order_sum}\n
-        Average (mean) order value: {order_sum / order_total}\n
-        Max amount: {max(order_amounts)}\n
-        Min amount: {min(order_amounts)}\n
-
-        Summary stats by customer:
-        {[item for item in customer_order_summary]}
-        """)
+    try:
+        payload_dict = json.loads(msg_payload)
+        order_id = str(payload_dict["order_id"])
+        amount = payload_dict["amount"]
+        customer_id = str(payload_dict["customer_id"])
     
-    
-def start_consume_loop(consumer, topic):
+        order_total += 1
+        order_sum += int(amount)
+        order_amounts.append(amount)
+        print(customer_order_summary.keys())
+        print(customer_id in customer_order_summary.keys())
+        customer_key = f"Orders for Customer ID {customer_id}"
+        if customer_key in customer_order_summary.keys():
+            customer_order_summary[f"Orders for Customer ID {customer_id}"] += 1
+        else:
+            customer_order_summary[f"Orders for Customer ID {customer_id}"] = 1
+
+
+        logging.info(f"New Order Received! -- Order ID: {order_id} -- Customer ID: {customer_id} -- Amount: {amount}\n\n")
+        logging.info(f"Total Orders: {order_total} -- Total Sales: {order_sum} -- Average Order: {order_sum / order_total}")
+        logging.info(f"Smallest order: {min(order_amounts) if min(order_amounts) > 0 else 0} ")
+        logging.info(f"Largest order: {max(order_amounts) if max(order_amounts) > 0 else 0}")
+        logging.info(f"Customer Order Summary: {customer_order_summary}")
+
+
+    except json.JSONDecodeError as e:
+        logging.error(f"Error deconding JSON message {msg.value()}. Error: {e}")
+
+
+def start_consume_loop(topic):
+
+    consumer = Consumer(kafka_consumer_config)
+
     try: 
         consumer.subscribe(topic)
         while running:
@@ -70,14 +72,19 @@ def start_consume_loop(consumer, topic):
                 continue
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
-                    sys.stderr.write(('%% %s [%d] reached end at offset %d\n' %(msg.topic(), msg.partition(), msg.offset())))
+                    logging.warning('%% %s [%d] reached end at offset %d\n' %(msg.topic(), msg.partition(), msg.offset()))
             elif msg.error():
                 raise KafkaException(msg.error())
             else:
                 msg_process(msg)
                 consumer.commit(asynchronous=False)
+    except KeyboardInterrupt:
+        logging.info("Consumer stopped.")
     finally:
         consumer.close()
 
-start_consume_loop(consumer, ['online-orders'])
 
+if __name__ == "__main__":
+    logging.info("Starting Kafka Consumer...")
+    
+    start_consume_loop(["online-orders"])
